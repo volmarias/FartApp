faee726cb0cef71ecff6d1c6ed7575526fa811c3
volmarias <volmarias@volmari.as>
2022-12-29 12:13:57 -0500

    Initial functionality

--- app/src/main/java/as/volmari/fartapp/MainActivity.kt
+++ app/src/main/java/as/volmari/fartapp/MainActivity.kt
@@ -1,18 +1,26 @@
 package `as`.volmari.fartapp
 
+import `as`.volmari.fartapp.ui.theme.FartAppTheme
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import `as`.volmari.fartapp.ui.theme.FartAppTheme
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,22 +30,45 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Poop {
+                        playFart()
+                    }
                 }
             }
+        }
+    }
+
+    fun playFart() {
+        val fart = when(Random.nextInt(1..4)) {
+                1 -> R.raw.fart_01
+                2 -> R.raw.fart_02
+                3 -> R.raw.fart_03
+                4 -> R.raw.fart_04
+            else -> R.raw.fart_01
+        }
+        MediaPlayer.create(this, fart).apply {
+            this.setOnCompletionListener { this.release() }
+            start()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Poop(click: () -> Unit = {}) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Button(onClick = click, modifier = Modifier.align(Center)) {
+            Image(
+                painter = painterResource(id = R.drawable.noto_emoji_kitkat_1f4a9),
+                contentDescription = "poo",
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     FartAppTheme {
-        Greeting("Android")
+        Poop {}
     }
 }
